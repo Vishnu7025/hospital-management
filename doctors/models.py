628755a749d4deb56c66a282b5cadd6a878da824
Vishnu  V S <Vishnuviswambaran2002@gmail.com>
2022-08-24 15:25:16 +0530

    admin verbose change

--- doctors/models.py
+++ doctors/models.py
@@ -1,4 +1,5 @@
 import imp
+from tabnanny import verbose
 from django.db import models
 
 from facility import views
@@ -11,6 +12,10 @@ class Doctors(models.Model):
     doc_spec = models.CharField(max_length=255)
     dep_name = models.ForeignKey(Departments,on_delete=models.CASCADE)
     doc_image = models.ImageField(upload_to='doctors')
+
+    class Meta:
+        verbose_name= 'Doctors'
+        verbose_name_plural  = 'Doctors'
 
     def __str__(self):
         return 'Dr ' +  self.doc_name + ' - (' + self.doc_spec + ')'
--- facility/models.py
+++ facility/models.py
@@ -1,4 +1,4 @@
-from typing_extensions import Self
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -6,6 +6,9 @@ class Departments(models.Model):
     dep_name = models.CharField(max_length=155)
     dep_desc = models.TextField()
 
+    class Meta:
+        verbose_name ='Departments'
+        verbose_name_plural= 'Departments'
     def __str__(self):
         return self.dep_name
    
